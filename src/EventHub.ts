85357b81219f58c8e33f39320d2db4ee8e984ba9
pomelo <goodmanluoxin@163.com>
2019-10-16 21:14:07 +0800

    借鉴 eventListener ，给 handler 抽象成 listener

--- src/EventHub.ts
+++ src/EventHub.ts
@@ -1,21 +1,36 @@
 
 interface Emiter { }
 
+class EventListener {
+  public emitter: EventHub
+  public handler: () => void
+  constructor(handler: () => void, emitter: EventHub) {
+    this.handler = handler
+    this.emitter = emitter
+  }
+}
 
 export class EventHub {
-  private cached: { [name: string]: Array<() => void> } = {}
+  private cached: { [name: string]: EventListener[] } = {}
+
   public on(eventName: string, handler: () => void) {
+    if (!handler) {
+      throw new Error('invaild handler')
+    }
     if (!this.cached[eventName]) {
       this.cached[eventName] = []
     }
-    this.cached[eventName].push(handler)
+    const listener = new EventListener(handler, this)
+    this.cached[eventName].push(listener)
   }
+
   public emit(eventName: string) {
-    const handlers = this.cached[eventName]
-    handlers.forEach(handler => {
-      handler()
+    const listeners = this.cached[eventName]
+    listeners.forEach((listener) => {
+      listener.handler.call(this)
     })
   }
+
   public off(eventName: string) {
     this.cached[eventName] = null
   }
